e25140c67198aa0ad3899283d4912b517ce3b216
Mark deGroat <mhdegroat@gmail.com>
2017-03-12 15:53:52 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,22 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array_of_keys = []
+    arguments.each do |argument|
+      self.each do |key, value|
+        if value == argument
+          array_of_keys.push(key)
+        end
+      end
+    end
+    array_of_keys
   end
 end
+
+# example_hash = {
+#   :here => "we go",
+#   :cant => "stop me now",
+#   :wont => "give you up"
+# }
+
+#puts example_hash.keys_of("we go")
